a359ade21107dc5a459cb574fcbb27947844cfb2
VIVEK KUMAR <106121146@nitt.edu>
2025-01-08 20:53:42 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://marketplace-6tji.vercel.app/' : 'http://localhost:5000',
+  baseURL: 'https://marketplace-6tji.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
