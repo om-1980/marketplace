5c0260c980397f36d80a051ca6dc8bdf81e144d2
VIVEK KUMAR <106121146@nitt.edu>
2025-01-08 19:55:39 +0530

    Update airtable.js

--- backend/utils/airtable.js
+++ backend/utils/airtable.js
@@ -1,7 +1,7 @@
 const Airtable = require('airtable');
 
-const airtableApiKey = process.env.AIRTABLE_API_KEY;
-const airtableBaseId = process.env.AIRTABLE_BASE_ID;
+const airtableApiKey = patkEXwIwCyfN6sdJ.c4530ab9389610f2879210c33188c8cdd3a0b12ecf31e54bad21106fd88f4d23;
+const airtableBaseId = appFkXEQOQRyieIqN;
 
 if (!airtableApiKey || !airtableBaseId) {
   throw new Error('Airtable API Key or Base ID is missing.');
